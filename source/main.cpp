386594449cc472b2a94d324d9693697020755a72
damien <damienhall04@gmail.com>
2022-02-01 19:35:25 +0000

    multiple boxes and fun business

--- source/main.cpp
+++ source/main.cpp
@@ -15,53 +15,58 @@ int main(void) {
 	videoSetMode(MODE_5_3D);
 	consoleDemoInit();
 	glScreen2D();
-
 	bool gradient = true;
+	std::vector<Box> boxes;
 	Box _box;
-
+	boxes.push_back(_box);
 	while(1) {
 		glBegin2D();		
 		scanKeys();
 
 		int keys = keysDown();
 		if (keys & KEY_A) {
-			_box.Randomise();
+			boxes[0].Randomise();
 		}
 		if (keys & KEY_L){ // decrease speed
-			_box.AdjustSpeed(0);
+			boxes[0].AdjustSpeed(0);
 		}
 		if (keys & KEY_R){ // increase speed
-			_box.AdjustSpeed(1);
+			boxes[0].AdjustSpeed(1);
 		}
 		if (keys & KEY_LEFT){
-			_box.DecreaseSize(1,0);
+			boxes[0].DecreaseSize(1,0);
 		}
 		if (keys & KEY_RIGHT){ // increase width when RIGHT pressed
-			_box.IncreaseSize(1,0);
+			boxes[0].IncreaseSize(1,0);
 		}
 		if (keys & KEY_UP){	// decrease height when UP pressed
-			_box.DecreaseSize(0,1);
+			boxes[0].DecreaseSize(0,1);
 		}
 		if (keys & KEY_DOWN){ // increase height when DOWN pressed
-			_box.IncreaseSize(0,1);
+			boxes[0].IncreaseSize(0,1);
 		}
-		if (keys & KEY_SELECT){ // reset values
+		if (keys & KEY_Y){ // reset values
+			Box _newBox;
+			boxes.push_back(_newBox);
 		}
 		if (keys & KEY_START){
 			gradient = !gradient;
 		}
 
-		_box.Update();
-		_box.Draw(gradient);
-
-		printf("\x1b[3;0Hbouncing dvd test - damien :)");
-		printf("\x1b[5;0Hpress A to randomise");
-		printf("\x1b[6;0HL + R to adjust speed");
-		printf("\x1b[7;0HLeft + Right to adjust width");
-		printf("\x1b[8;0HUp + Down to adjust height");
-		printf("\x1b[9;0HSELECT to reset values");
-		printf("\x1b[10;0HSTART to switch colour mode");
-		_box.PrintDebugInfo();
+		for (int i =0; i < boxes.size(); i++)
+		{
+			boxes[i].Update();
+			boxes[i].Draw(gradient);
+		}
+		printf("\x1b[2;0Hbouncing dvd test - damien :)");
+		printf("\x1b[4;0Hpress A to randomise");
+		printf("\x1b[5;0HL + R to adjust speed");
+		printf("\x1b[6;0HLeft + Right to adjust width");
+		printf("\x1b[7;0HUp + Down to adjust height");
+		printf("\x1b[8;0HSELECT to reset values");
+		printf("\x1b[9;0HSTART to switch colour mode");
+		printf("\x1b[10;0HY to create new box");
+		boxes[0].PrintDebugInfo();
 
 		glEnd2D();
 		glFlush(0);
